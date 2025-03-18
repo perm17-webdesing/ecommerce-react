94c1c3a935b2d427bfee485d91463fbcbe279e7f
Pedro Rivero <perm17@gmail.com>
2025-03-17 21:38:26 -0300

    Correcion de json vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ecommerce-react/", // 👈 Este debe coincidir con el nombre de tu repo en GitHub
 })
