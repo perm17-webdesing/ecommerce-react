e6de77b90864be9b4fb38987847c81aae5cedc4e
Pedro Rivero <perm17@gmail.com>
2025-03-20 20:59:17 -0300

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/ecommerce-react/", // 👈 Asegúrate de que sea el nombre exacto de tu repositorio
-})
